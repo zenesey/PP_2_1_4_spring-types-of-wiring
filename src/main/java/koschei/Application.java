36e8c38e0bf905f792a8b915e54b530ecb099226
zEnesy <zenesy13@gnail.com>
2022-07-07 17:04:40 +0300

    SAS

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -6,10 +6,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        KoscheiTheDeathless koscheiTheDeathless =
-                applicationContext.getBean(KoscheiTheDeathless.class);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        KoscheiTheDeathless koscheiTheDeathless =  applicationContext.getBean(KoscheiTheDeathless.class);
+
         System.out.println(koscheiTheDeathless.getRulesByDeth());
     }
 }
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,16 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+    @Autowired
+    private  Deth8 deth;
+//    @Autowired
+//    public Needle7(Deth8 deth) {
+//        this.deth = deth;
+//    }
 
     @Override
     public String toString() {
